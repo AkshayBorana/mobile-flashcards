4a0f19abc30c6e82fc5f2d94f6c904f5015cabc0
Akshay Borana <akshayborana1@gmail.com>
2020-06-19 20:07:30 -0400

    feat: added actions creators for creating a a card and getting all the decks.

--- actions/decks.js
+++ actions/decks.js
@@ -1,8 +1,57 @@
 export const ADD_DECK = 'ADD_DECK';
+export const ADD_CARD = "ADD_CARD";
+export const RECEIVE_DECKS = "RECEIVE_DECKS";
+import { saveDeck, saveCard, getDecks } from "../utils/API";
 
-export function addDeck(title) {
+export function addDeck(deck) {
   return {
     type: ADD_DECK,
-    title
+    deck
   };
 }
+
+export function addCardToDeck(deckId, card) {
+  return {
+    type: ADD_CARD,
+    deckId,
+    card
+  };
+}
+
+export function receiveDeck(decks) {
+  return {
+    type: RECEIVE_DECKS,
+    decks
+  }
+}
+
+export function handleAddDeck(name, calBackFunc) {
+  return dispatch => {
+    const deck = {
+      name,
+      id: Date.now(),
+      cards: []
+    };
+    saveDeck(deck).then(() => {
+      dispatch(addDeck(deck));
+      calBackFunc(deck);
+    });
+  };
+}
+
+export function handleAddCardToDeck(deckId, card) {
+  return dispatch => {
+    const newCard = {
+      ...card,
+      id: Date.now()
+    };
+    saveCard(deckId, card).then(() => dispatch(addCardToDeck(deckId, newCard)));
+  };
+}
+
+export function handleInitialData() {
+  return dispatch => {
+    getDecks().then(decks => {
+      dispatch(addDecks(decks || []));
+    });
+};
